4047a936f55572df41fb33cce43d858f19ca58ff
Dan <decisivedaniel@proton.me>
2022-12-02 20:20:45 +0100

    Part 1 working

--- 1/calorieCounting.cpp
+++ 1/calorieCounting.cpp
@@ -1,27 +1,36 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int input()
 {
+    std::ifstream input("input.txt");
     int maximum = 0;
-    int current = 0;
+    std::string current;
     int currentElfTotal = 0;
-    while (current >= 0)
+    int elf = 1;
+    int maxElf = 0;
+    while (getline(input, current))
     {
-        std::cin >> current;
-        if (current > 0)
+        //std::cout << current << std::endl;
+        if (current != "")
         {
-           currentElfTotal += current;
+           currentElfTotal += stoi(current);
         }
-        if (current == 0)
+        else
         {
             if (currentElfTotal > maximum)
             {
-                maximum == currentElfTotal;
-                currentElfTotal = 0;
+                maximum = currentElfTotal;
+                maxElf = elf;
+                std::cout << maximum << std::endl;
             }
+            currentElfTotal = 0;
+            elf++;
         }
     }
-
+    input.close();
+    std::cout << maxElf << "has ";
     return maximum;
 }
 
