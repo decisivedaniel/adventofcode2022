94a8502cc6e4d034a1a132c9bb9f826c08169006
Dan <decisivedaniel@proton.me>
2022-12-03 16:48:21 +0100

    Clean up the code with some refactoring

--- 3/rucksackReorg.py
+++ 3/rucksackReorg.py
@@ -1,5 +1,9 @@
 
-
+def findPosition(char):
+    if ord(char) > 91:
+        return ord(char) - ord("a")
+    else:
+        return ord(char) - ord("A") + 26
 
 f = open("input.txt")
 conflicts = [0] * 52
@@ -11,29 +15,13 @@ for line in lines:
     halfLength = len(line) / 2
     currentPosition = 0
     for char in line.strip("\n"):
-        #print(char)
         if currentPosition < halfLength - 1:
-            if ord(char) > 91:
-                #print(" first lc \n")
-                firstCompartment[ord(char) - ord("a")] += 1
-            else:
-                #print(" first uc \n")
-                firstCompartment[ord(char) - ord("A") + 26] += 1
+            firstCompartment[findPosition(char)] += 1
         else:
-            if ord(char) > 91:
-                #print(" second lc \n")
-                secondCompartment[ord(char) - ord("a")] += 1
-            else:
-                #print(" second uc \n")
-                secondCompartment[ord(char) - ord("A") + 26] += 1
+            secondCompartment[findPosition(char)] += 1
         currentPosition += 1
     for x in range(52):
         if firstCompartment[x] > 0 and secondCompartment[x] > 0:
-            if x < 26:
-                foundConflict = chr(x + ord("a"))
-            else:
-                foundConflict = chr(x + ord("A"))
-            #print("conflict ", foundConflict)
             conflicts[x] += 1
             break
 for x in range(52):
